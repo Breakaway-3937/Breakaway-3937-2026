ed69ec5b81098b869e2172c48714d2dab2653d1b
Ethan <emccown@harding.edu>
2026-02-28 23:17:35 -0600

    ocd

--- src/main/java/frc/robot/utility/Constants.java
+++ src/main/java/frc/robot/utility/Constants.java
@@ -4,7 +4,6 @@ import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.RadiansPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 
-import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
