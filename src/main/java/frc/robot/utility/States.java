385d2041f8f3fad1c1e454f5433aea88f761d357
Ethan McCown <emccown@harding.edu>
2026-04-20 15:51:09 -0500

    FAST LITTLE INTAKE

--- src/main/java/frc/robot/utility/States.java
+++ src/main/java/frc/robot/utility/States.java
@@ -6,8 +6,8 @@ public class States {
         STOW(400.0, 0.0),
         FIRE(212.0,21.0),
         IDLE(212.0, 0.0),
-        INTAKE(0.5, 21.0),
-        UNCLOG(0.5, -21.0);
+        INTAKE(0.5, 60.0),
+        UNCLOG(0.5, -60.0);
 
         private final double angle, power;
         private final double INTAKE_POWER_CONVERSION = 1.67;
